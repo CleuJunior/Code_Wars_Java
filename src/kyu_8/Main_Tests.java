9fa2fcf5eea517190a81ba959a5343ef2a093f54
CleuJunior <cleu.junior@gmail.com>
2020-03-28 19:58:37 -0300

    Kyu 8 Positivos para Negativos, Negativos para Positivos

--- src/kyu_8/Main_Tests.java
+++ src/kyu_8/Main_Tests.java
@@ -1,12 +1,14 @@
 package kyu_8;
 
+import java.util.Arrays;
+
 public class Main_Tests {
 
+
 	public static void main(String[] args) {
+		int[] array = { 1, -1, -2, 2, 3, -4 };
 
-		int[] arg = { 78, 56, 232, 12, 11, 43, 0, -3 };
-
-		System.out.println(kataMenorNaArray.findSmallestInt(arg));
+		System.out.println(Arrays.toString(kataInverteValor.invert(array)));
 
 	}
 
