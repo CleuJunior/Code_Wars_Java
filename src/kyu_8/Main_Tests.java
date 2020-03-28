15821ce79a27f1f55d7d96f3aa1293d39eef2826
CleuJunior <cleu.junior@gmail.com>
2020-03-28 02:20:30 -0300

    Projeto de Lógica de Programação

---
+++ src/kyu_8/Main_Tests.java
@@ -0,0 +1,14 @@
+package kyu_8;
+
+public class Main_Tests {
+
+	public static void main(String[] args) {
+
+		int[] array = { 1, 2, 3, 4 };
+		int n = 2;
+
+		System.out.println(kataPower.nthPower(array, n));
+
+	}
+
+}
