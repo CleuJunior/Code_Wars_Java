dbeeb7ad095e78cae63e6b4b5f06597a2b852655
CleuJunior <cleu.junior@gmail.com>
2020-03-28 03:30:36 -0300

    Desafio Kyu 8 Adicionado

--- src/kyu_8/Main_Tests.java
+++ src/kyu_8/Main_Tests.java
@@ -2,12 +2,11 @@ package kyu_8;
 
 public class Main_Tests {
 
-
 	public static void main(String[] args) {
 
-		String str = "asac";
-		
-		System.out.println(kataReversedString.solution(str));
+		String str = "eloquent";
+
+		System.out.println(RemoveChars.remove(str));
 
 	}
 
---
+++ src/kyu_8/RemoveChars.java
@@ -0,0 +1,15 @@
+package kyu_8;
+
+/*
+ * It's pretty straightforward. Your goal is to create a function that removes the first and last characters of a string.
+ * You're given one parameter, the original string. You don't have to worry with strings with less than two characters.
+ */
+
+public class RemoveChars {
+	public static String remove(String str) {
+		String newStr = str.substring(1, str.length() - 1);
+		return newStr;
+
+	}
+
+}
